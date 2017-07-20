be8b90ce09a830e4b8f910699a69c815aafcfb30
Nikita Singh <nikita_singh@external.mckinsey.com>
2017-07-20 14:13:43 +0100

    remove https tp support cdn

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const app = express();
 // redirect that request to the
 // same url but with HTTPS
 const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(['https://', req.get('Host'), req.url].join(''));
+    return function(req, res, next) {
+        // if (req.headers['x-forwarded-proto'] !== 'https') {
+        //   return res.redirect(['https://', req.get('Host'), req.url].join(''));
+        // }
+        next();
     }
-    next();
-  }
 }
 
 // Instruct the app
@@ -27,7 +27,7 @@ app.use(express.static(__dirname + '/dist'));
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Start the app by listening on the default
